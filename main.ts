83d56f1e30339976a461f897026118ba152a7c17
Anluhen <andrehenc@gmail.com>
2025-10-17 10:11:32 -0300

    refactor: change plugin name

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { Plugin } from 'obsidian';
 
 import { createDragHandleExtension } from "./src/drag-handle-extension";
 
-export default class DragAndDropPlugin extends Plugin {
+export default class DragAndDropLines extends Plugin {
 	async onload() {
 		this.registerEditorExtension(createDragHandleExtension());
 	}
